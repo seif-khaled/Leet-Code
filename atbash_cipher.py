1b81a6ec8849be2bd70f87f989abc974cf705465
seif-khaled <seifkhaled2424@gmail.com>
2023-08-16 18:05:23 +0300

    added file

---
+++ atbash_cipher.py
@@ -0,0 +1,34 @@
+def atbash_cipher(st):
+    s=""
+    for i in range(len(st)):
+        flag=0
+        if ord(st[i])>=65 and ord(st[i])<=90:
+            x=st[i].lower()
+            flag=1
+        if flag==1:
+            if ord(x)>=97 and ord(x)<97+13:
+                diffrence=ord(x)-97
+                x=chr(ord(x)+(25-diffrence-diffrence))
+                s+=x.upper()
+            elif ord(x)>97+13 and ord(x)<=122:
+                diffrence=122-ord(x)
+                x=chr(ord(x)-(25-diffrence-diffrence))
+                s+=x.upper()
+            elif ord(x)==97+13:
+                s+=x.upper()
+        elif ord(st[i])>=97 and ord(st[i])<97+13:
+            diffrence=ord(st[i])-97
+            # print(diffrence)
+            s+=chr(ord(st[i])+(25-diffrence-diffrence))
+        elif ord(st[i])>97+13 and ord(st[i])<=122:
+            diffrence=122-ord(st[i])
+            # print(diffrence)
+            s+=chr(ord(st[i])-(25-diffrence-diffrence))
+        elif ord(st[i]) ==97+13:
+            s+=st[i]
+        else:
+            s+=st[i]   
+    return s
+print(atbash_cipher("apple")=="zkkov") 
+print(atbash_cipher("Hello world!")=="Svool dliow!") 
+print(atbash_cipher("Christmas is the 25th of December")=="Xsirhgnzh rh gsv 25gs lu Wvxvnyvi") 
--- increasing triplet sequence.py
+++ increasing triplet sequence.py
@@ -1,2 +1,29 @@
+import math
+
 def increasingTriplet(nums):
-    for i in range()
+    num1=-2147483648800
+    num2=-2147483648800
+    num3=-2147483648800
+    for i in range(len(nums)):
+        if i==0:
+            num1=nums[i]
+        else:
+            if nums[i]> num1 and nums[i]> num2 :
+                return True,num1,num2
+            if nums[i]>num1:
+                num2=nums[i]
+            elif nums[i] > num1 and nums[i]< num2:
+                num2=nums[i]
+            elif nums[i]<num1:
+                num1=nums[i]
+    return False
+
+
+print(increasingTriplet([5,4,3,2,1]))
+print(increasingTriplet([2,1,5,0,4,6]))
+print(increasingTriplet([20,100,10,12,5,13]))
+print(increasingTriplet([2,4,-2,-3]))
+# x=None
+# y=-2147483648800
+
+# print(y)
