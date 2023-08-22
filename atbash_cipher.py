88965fee493e572be0df0d8f7fb6fbb551292ccb
seif-khaled <seifkhaled2424@gmail.com>
2023-08-22 11:02:34 +0300

    edited file

--- atbash_cipher.py
+++ atbash_cipher.py
@@ -15,6 +15,7 @@ def atbash_cipher(st):
                 x=chr(ord(x)-(25-diffrence-diffrence))
                 s+=x.upper()
             elif ord(x)==97+13:
+                x=chr(ord(x)-1)
                 s+=x.upper()
         elif ord(st[i])>=97 and ord(st[i])<97+13:
             diffrence=ord(st[i])-97
@@ -25,10 +26,13 @@ def atbash_cipher(st):
             # print(diffrence)
             s+=chr(ord(st[i])-(25-diffrence-diffrence))
         elif ord(st[i]) ==97+13:
-            s+=st[i]
+            s+=chr(ord(st[i])-1)
         else:
             s+=st[i]   
     return s
 print(atbash_cipher("apple")=="zkkov") 
 print(atbash_cipher("Hello world!")=="Svool dliow!") 
 print(atbash_cipher("Christmas is the 25th of December")=="Xsirhgnzh rh gsv 25gs lu Wvxvnyvi") 
+print(atbash_cipher("abcdefghijklmnopqrstuvwxyz"))
+print(atbash_cipher("ABCDEFGHIJKLMNOPQRSTUVWXYZ"))
+print(atbash_cipher("Vmxibkgrlm zmw wvxibkgrlm ziv rwvmgrxzo uli gsv Zgyzhs xrksvi."))
