2eda49b2b6e0e1fee1be994970b0a5394d76f972
seif-khaled <seifkhaled2424@gmail.com>
2023-11-22 22:31:02 +0200

    added some files

--- recursion_practice.py
+++ recursion_practice.py
@@ -45,3 +45,147 @@ def reverse_num(num,res=0):
 #
 
 # print(9646324351>(2**31)-1)
+###################3
+#fibonacci sequcne 
+def fib(n):
+		if n<=1:
+			return n
+		return fib(n-2)+fib(n-1)
+
+#print(fib(15))
+
+#count the digits of number integres only
+def count(n):
+	if n<=1:
+		return int(n)
+	return 1+count(n/10) 
+	
+#print(count(51036692461890443212))
+
+#print array eleemnts 
+
+def print_array(a,n=0):
+	if n==len(a):
+		return 
+	else:
+		print(a[n])
+		return print_array(a,n+1)
+	    
+	
+#print(print_array([1,2,3,4,7]))
+
+def sum_of_digits(n):
+	if n<=1:
+		return n
+	else:
+		return n%10+sum_of_digits((n)//10)
+
+
+#print(sum_of_digits(1234))
+
+
+#######
+def reverse_num(num,res=0):
+	if num<=1:
+		rem=num%10
+		res=res+rem
+		return res
+	else:
+		rem=num%10
+		res=res*10+rem
+		print(res)
+		return reverse_num(num//10,res)
+		
+		
+
+#print(reverse_num(1234))
+
+
+def factorial(n):
+	if n<=1:
+		return 1
+	return n*factorial(n-1)
+#print(factorial(5))
+
+
+
+###
+def is_p(n,x):
+	if n==1 or n==0:
+		return False
+	elif x==1:
+		return True
+	if n%x==0:
+		return False
+	return is_p(n,x-1)
+	
+	
+#print(is_p(9,8))
+
+
+def remove_adjacentduplicates_string(s,n,l):
+    if n==len(s)-1:
+        l+=s[n]
+        return l
+    elif s[n] !=s[n+1]:
+        l+=s[n]
+    return remove_adjacentduplicates_string(s,n+1,l)
+
+
+
+string="aabc"
+string3="AABBCCDDEE"
+string3="effgee"
+# lenght=len(string)
+l=[]
+
+# print(remove_adjacentduplicates_string("aabcx",0,""))
+# # l=[] 
+# print(remove_adjacentduplicates_string("kosmmm",0,""))
+# #l=[]   
+# print(remove_adjacentduplicates_string("AABBCCDDEE",0,""))   
+# #l=[]     
+# print(remove_adjacentduplicates_string("effgee",0,""))
+
+
+# ######
+# def gcd(a,b):
+# 	return a%gcd(a-1,b-1)     
+
+
+######
+def mul(a,b,n):
+    if n==max(a,b):
+        return min(a,b)
+    return min(a,b)+mul(a,b,n+1)
+
+
+# print(mul(5,2,1))
+
+
+
+################################
+
+def odd_number_sum(l,n):
+    if n==len(l):
+        return 0
+    if l[n]%2!=0:
+        return l[n]+odd_number_sum(l,n+1)
+    else:
+        return odd_number_sum(l, n + 1)
+    
+    
+    
+# print(odd_number_sum([1,2,3,4],0))
+# # print(odd_number_sum([1,2,3,4],0))
+# print(odd_number_sum([2,3,6,9],0))
+# print(odd_number_sum([],0))
+
+
+#############
+
+def sum_till_one(n):
+        
+		
+		 
+print(sum_till_one(38))
--- rev_int.py
+++ rev_int.py
@@ -1,30 +1,68 @@
-#reverse integrr using loop 
+# #reverse integrr using loop 
 
-def rev_int(x):
-    flag=0
-    if x<0:
-        flag=1
-        x=x*-1
-    res=0
-    rem=0
-    while((x>=1) or x//10!=0 ):
-        rem = x % 10
-        res=(res*10)+rem
-        x//=10
-    if flag:
-        res=res*-1
-    if res>(2**31)-1 or res<-2**31:
-        return 0
-    return res
+# def rev_int(x):
+#     flag=0
+#     if x<0:
+#         flag=1
+#         x=x*-1
+#     res=0
+#     rem=0
+#     while((x>=1) or x//10!=0 ):
+#         rem = x % 10
+#         res=(res*10)+rem
+#         x//=10
+#     if flag:
+#         res=res*-1
+#     if res>(2**31)-1 or res<-2**31:
+#         return 0
+#     return res
         
-# print(rev_int(-5234))
-# print(rev_int(-150))
-# print(rev_int(250))
-# print(rev_int(4321))
-# print(rev_int(-156))
-# print(rev_int(673))
-# print(rev_int(-123))
-# print(rev_int(17596))
-# print(rev_int(97596))
-# print(rev_int(10774302610000000))
-print(rev_int(1534236469))
+# # print(rev_int(-5234))
+# # print(rev_int(-150))
+# # print(rev_int(250))
+# # print(rev_int(4321))
+# # print(rev_int(-156))
+# # print(rev_int(673))
+# # print(rev_int(-123))
+# # print(rev_int(17596))
+# # print(rev_int(97596))
+# # print(rev_int(10774302610000000))
+# print(rev_int(1534236469))
+
+
+
+# def sec_big(l):
+#     biggest=l[0]
+#     bigget_2nd=-1
+#     for i in range(1,len(l)):
+#         if l[i]>biggest:
+#             bigget_2nd=biggest
+#             print(biggest)
+#             biggest=l[i]
+#             print(biggest)
+#         elif l[i]<biggest and l[i]>bigget_2nd:
+#             bigget_2nd=l[i]
+#         # print(biggest,bigget_2nd)
+#     return biggest,bigget_2nd
+    # l.remove(max(l))
+    # # biggest=l[0]
+    # # for i in range(1,len(l)):
+    # #     if l[i]>biggest:
+    # #         biggest=l[i]
+    # # l.remove(biggest)
+    # sec_big=l[0]
+    # for i in range(1,len(l)):
+    #     if l[i]>sec_big:
+    #         sec_big=l[i]
+            
+    # return sec_big
+
+
+# print(sec_big([4,3,2,5]))
+# print(sec_big([13,3,2,5]))
+# print(sec_big([12,3,6,5,10]))
+# print(sec_big([13,8,2,5]))
+        
+            
+        
+            
